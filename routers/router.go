983de460366a9f09f4b189fbba1a18e42d673700
Joaquin Mendez <joaquin.enmt@gmail.com>
2025-12-23 17:33:01 -0300

    Fix: Eliminado endpoint en desuso

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,8 @@
 package routers
 
 import (
-	"log"
-
 	h "github.com/juackomdz/control-asistencia/handlers"
-	"github.com/juackomdz/control-asistencia/middleware"
+	m "github.com/juackomdz/control-asistencia/middleware"
 	"github.com/labstack/echo/v4"
 )
 
@@ -16,20 +14,11 @@ func RouterPath(e *echo.Echo) {
 	api.POST("/register", h.Registro)
 	api.POST("/login", h.Ingreso)
 
-	a := api.Group("/auth", middleware.MiddleJWT())
+	a := api.Group("/auth", m.MiddleJWT())
 	//Rutas asistencia
 	a.POST("/check-in", h.CheckIn)
 	a.POST("/check-out", h.CheckOut)
 
 	//Rutas estadisticas
 	a.GET("/data/:user_id", h.ResumenHoras)
-
-	api.GET("/test", func(c echo.Context) error {
-
-		tok := middleware.GeneraJWT("correo@correo.cl", 1)
-
-		log.Print(tok)
-		return c.JSON(200, "ok")
-	}, middleware.MiddleJWT())
-
 }
